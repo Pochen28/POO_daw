cc9d914797bf51234e8cd8052f69958de1eac1f6
Jose <rieraljoseluis202@gmail.com>
2026-01-23 16:30:40 +0100

    Libros

--- POO_daw/src/main/java/org/example/Libros.java
+++ POO_daw/src/main/java/org/example/Libros.java
@@ -4,19 +4,67 @@ public class Libros {
 
     private String titulo;
     private String autor;
-    private int id;
+    private String id;
     private boolean disponible;
 
-    public static int cantidadLibros = 0;
+    private static String prefijoId = "LIB";
+    private static int totalLibros = 0;
+    private static int stockDisponible = 0;
 
-    public static int librosDisponibles = 0;
-
-
-    public Libros (String titulo, String autor){
+    public Libros(String titulo, String autor) {
         this.titulo = titulo;
         this.autor = autor;
+        this.disponible = true;
 
+        totalLibros++;
+        stockDisponible++;
+        this.id = prefijoId;
     }
 
-}
+    public void Prestar() {
+        if (this.disponible) {
+            this.disponible = false;
+            stockDisponible--;
+            System.out.println("El libro " + this.titulo + " ha sido prestado.");
+        } else {
+            System.out.println("Error! Ya esta prestado.");
+        }
+    }
 
+    public void devolver() {
+        if (!this.disponible) {
+            this.disponible = true;
+            stockDisponible++;
+            System.out.println("Has devuelto " + this.titulo + ", Gracias!");
+        } else {
+            System.out.println("Este libro ya esta en la estanteria.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String situacion = this.disponible ? "Disponible" : "Prestado";
+
+        return "LIBRO [" + this.id + "] " + "Título: " + this.titulo + " | " + "Autor: " + this.autor + " | " + "Estado: " + situacion;
+    }
+
+    private String generarId() {
+        return "LIB " + totalLibros;
+    }
+
+    public boolean estaDisponible() {
+        return this.disponible;
+    }
+
+    public static int getCantidadLibros() {
+        return totalLibros;
+    }
+
+    public static int getLibrosDisponible() {
+        return stockDisponible;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
