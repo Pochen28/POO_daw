56cf44d66f2c3963bb0ac29147de636fa08b8416
Jose <rieraljoseluis202@gmail.com>
2026-03-05 20:23:56 +0100

    no message

---
+++ POO_daw/src/main/java/org/example/EjercicioAceptaElReto.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+
+public class EjercicioAceptaElReto {
+        public class solution {
+            static java.util.Scanner in;
+            Stack <Character>pila = new Stack<>();
+
+            String expression = in.nextLine();
+            public static boolean casoDePrueba () {
+                if (!in.hasNext()){
+
+                }
+                return false;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
